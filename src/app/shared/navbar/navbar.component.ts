25a7239c16af75fc8ec9ba6925787590fb21d7b1
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-12-03 13:37:58 -0500

    feat: adding sticky for navbar and burguer menu

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   standalone: true,
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit{
+
+  ngAfterViewInit() {
+    const nav = document.querySelector('nav');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const mainContent = document.querySelector('main');
+
+    if (hamburgerMenu) {
+      hamburgerMenu.addEventListener('click', () => {
+        nav?.classList.toggle('active');
+        mainContent?.classList.toggle('blur');
+      });
+    }
+  }
 
 }
