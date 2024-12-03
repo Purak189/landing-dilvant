84e423cb8f41c12222178dacb40a707799f831ec
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-12-03 10:29:54 -0500

    chore: initial configuration with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeroSectionComponent} from './components/hero-section/hero-section.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeroSectionComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
